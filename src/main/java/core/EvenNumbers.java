468d126d1a80ab8632c85a91222cb3107ee75675
Bakhtiyor Makhsimov <bakhtiyor.makhsimov@gmail.com>
2017-08-02 20:49:11 -0700

    Adding two classes OddNumbers and EvenNumbers

---
+++ src/main/java/core/EvenNumbers.java
@@ -0,0 +1,14 @@
+package core;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		int max = 10;
+		for (int i = 0; i <= max; i++)
+			if (i % 2 == 0) {
+				System.out.println(i + ", ");
+			}
+
+	}
+
+}
---
+++ src/main/java/core/OddNumbers.java
@@ -0,0 +1,11 @@
+package core;
+
+public class OddNumbers 
+{
+    public static void main( String[] args )
+    {
+        int max = 10;
+        for (int i = 0; i <= max; i++)
+        	if (i % 2 != 0){System.out.println( i + ", ");}
+    }
+}
